1ba0d65b07044aa3428f586bd325a3902e5bcd77
Dinie Zikry <77114684+diniezikryy@users.noreply.github.com>
2022-08-28 00:07:45 +0800

    fixed routing for individual dashboards

--- pages/[user_id]/dashboard/[dashboard_id].tsx
+++ pages/[user_id]/dashboard/[dashboard_id].tsx
@@ -1,9 +1,59 @@
+import { collection, collectionGroup, getDocs } from "firebase/firestore";
 import { useRouter } from "next/router";
 import React from "react";
+import { db } from "../../../utils/firebase";
 
-const DashboardPage = () => {
+export async function getStaticPaths() {
+  const paths = [];
+  const boardsId = [];
+
+  const colRef = collection(db, "users");
+  const colSnap = await getDocs(colRef);
+
+  const boardSnap = await getDocs(collectionGroup(db, "boards"));
+  const boardPaths = boardSnap.docs.map((doc) => {
+    boardsId.push(doc.id.toString());
+  });
+
+  colSnap.docs.map((doc) => {
+    boardsId.forEach((boardId) => {
+      paths.push({
+        params: {
+          user_id: doc.id,
+          dashboard_id: boardId,
+        },
+      });
+    });
+  });
+
+  return {
+    paths: paths,
+    fallback: false,
+  };
+}
+
+export const getStaticProps = async (context: any) => {
+  const user_id = context.params.user_id;
+  const colRef = collection(db, "users", user_id, "boards");
+  const colSnap = await getDocs(colRef);
+
+  const allBoards = await getDocs(collectionGroup(db, "boards"));
+  return {
+    props: {
+      boards: colSnap.docs.map((doc) => doc.data()),
+      allBoards: allBoards.docs.map((board) => board.id),
+    },
+  };
+};
+
+const DashboardPage = ({ boards, allBoards }) => {
+  // This page should fetch the columns data from the firestore.
+
+  console.log(allBoards);
+
   const router = useRouter();
   const { user_id, dashboard_id } = router.query;
+
   return (
     <div>
       This is the user {user_id} first dashboard page of id {dashboard_id}.
--- pages/[user_id]/index.tsx
+++ pages/[user_id]/index.tsx
@@ -1,4 +1,34 @@
+import { collection, getDocs } from "firebase/firestore";
 import { useRouter } from "next/router";
+import { db } from "../../utils/firebase";
+
+export const getStaticPaths = async () => {
+  const colRef = collection(db, "users");
+  const colSnap = await getDocs(colRef);
+  const paths = colSnap.docs.map((doc) => {
+    console.log(doc.id.toString());
+    return {
+      params: { user_id: doc.id },
+    };
+  });
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+// Fetches all possible data of the specific user from firestore
+
+export const getStaticProps = async (context: any) => {
+  const id = context.params.user_id;
+  const colRef = collection(db, "users", id, "boards");
+  const colSnap = await getDocs(colRef);
+  return {
+    props: {
+      boards: colSnap.docs.map((doc) => doc.data()),
+    },
+  };
+};
 
 const UserPage = () => {
   const router = useRouter();
@@ -9,3 +39,5 @@ const UserPage = () => {
 };
 
 export default UserPage;
+
+// Generates all possible paths for baseUrl/:user_id/
